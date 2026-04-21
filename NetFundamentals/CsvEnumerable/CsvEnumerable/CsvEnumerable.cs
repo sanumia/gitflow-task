7c5fd8835996835d2a63b64d48cc0ee04b75bab7
Dziyana Haradzetskaya <diana21032006w@gmail.com>
2026-04-21 15:51:00 +0300

    add task csv enumerable

---
+++ NetFundamentals/CsvEnumerable/CsvEnumerable/CsvEnumerable.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper;
+using System.Collections;
+using System.Globalization;
+
+namespace CsvEnumerable;
+
+public class CsvEnumerable<T>(string filePath) : IEnumerable<T>
+{
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    public IEnumerator<T> GetEnumerator()
+    {
+        using (var reader = new StreamReader(filePath))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            var records = csv.GetRecords<T>();
+            foreach (var item in records)
+            {
+                yield return item;
+            }
+        }
+
+    }
+}
---
+++ NetFundamentals/CsvEnumerable/CsvEnumerable/Program.cs
@@ -0,0 +1,6 @@
+﻿using CsvEnumerable;
+
+string path = Path.Combine(AppContext.BaseDirectory, "Data", "user.csv");
+var users = new CsvEnumerable<User>(path);
+foreach (var user in users)
+    Console.WriteLine(user.Name);
