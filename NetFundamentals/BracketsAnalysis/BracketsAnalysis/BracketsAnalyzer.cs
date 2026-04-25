9efb234a00d57f22cca3030ab7ee89066189b150
Dziyana Haradzetskaya <diana21032006w@gmail.com>
2026-04-25 22:44:24 +0300

    update Bracket Analysis

--- NetFundamentals/BracketsAnalysis/BracketsAnalysis/BracketsAnalyzer.cs
+++ NetFundamentals/BracketsAnalysis/BracketsAnalysis/BracketsAnalyzer.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Metrics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BracketsAnalysis;
+﻿namespace BracketsAnalysis;
 
 public class BracketsAnalyzer
 {
